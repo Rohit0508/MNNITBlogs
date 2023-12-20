af6f7436bee726c807aa91df1075e82be05b5960
Rohit <rohitsingh.bigahi@gmail.com>
2023-12-20 09:43:57 +0530

    project setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className=' bg-red-400'>
+      helllo developer
+    </div>
+    </>
+  );
+}
+
+export default App;
