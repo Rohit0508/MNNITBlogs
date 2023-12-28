6ad2cd76b35506665fb0089e49315d4cdd483515
priyanshu5656 <priyanshukumar906@gmail.com>
2023-12-28 20:57:59 +0530

    n

--- server_side/models/OTP.js
+++ server_side/models/OTP.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 
 const OTPSchema = new mongoose.Schema({
     email:{
@@ -16,16 +17,26 @@ const OTPSchema = new mongoose.Schema({
         expires: 5*60,
     }
 });
+// Define a function to send emails
 async function sendVerificationEmail(email, otp) {
-    try{
-        const mailResponse = await mailSender(email, "Verification Email from Priyanshu", otp);
-        console.log("Email sent Successfully: ", mailResponse);
-    }
-    catch(error) {
-        console.log("error occured while sending mails: ", error);
-        throw error;
-    }
+	// Create a transporter to send emails
+
+	// Define the email options
+
+	// Send the email
+	try {
+		const mailResponse = await mailSender(
+			email,
+			"Verification Email",
+			emailTemplate(otp)
+		);
+		console.log("Email sent successfully: ", mailResponse.response);
+	} catch (error) {
+		console.log("Error occurred while sending email: ", error);
+		throw error;
+	}
 }
+
 
 OTPSchema.pre("save", async function(next) {
     await sendVerificationEmail(this.email, this.otp);
