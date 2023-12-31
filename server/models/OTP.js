51726382e3f6a0350fe0684b8c69d1dc33ebd6f8
priyanshu5656 <priyanshukumar906@gmail.com>
2023-12-31 16:46:45 +0530

    backend

---
+++ server/models/OTP.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
+
+const OTPSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required: true,
+    },
+    otp: {
+        type:String,
+        required:true,
+    },
+    createdAt: {
+        type:Date,
+        default:Date.now(),
+        expires: 5*60,
+    }
+});
+// Define a function to send emails
+async function sendVerificationEmail(email, otp) {
+	// Create a transporter to send emails
+
+	// Define the email options
+
+	// Send the email
+	try {
+		const mailResponse = await mailSender(
+			email,
+			"Verification Email",
+			emailTemplate(otp)
+		);
+		console.log("Email sent successfully: ", mailResponse.response);
+	} catch (error) {
+		console.log("Error occurred while sending email: ", error);
+		throw error;
+	}
+}
+
+
+OTPSchema.pre("save", async function(next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+}) 
+
+
+module.exports=mongoose.model("OTP",OTPSchema);
---
+++ server/routes/user.js
@@ -0,0 +1,47 @@
+
+const express = require("express")
+const router = express.Router();
+
+// Import the required controllers and middleware functions
+const {
+  login,
+  signup,
+  sendotp,
+//   changePassword,
+} = require("../controllers/auth");
+const { createPost } = require("../controllers/postController");
+
+const {createComment} =require("../controllers/commentConroller");
+const{likePost,unlikePost} =require("../controllers/likeController");
+const {
+    resetPasswordToken,
+    resetPassword,
+  } = require("../controllers/ResetPassword")
+
+const { auth } = require("../middlewares/auth")
+
+
+// Route for user login
+router.post("/login", login)
+
+// Route for user signup
+router.post("/signup", signup)
+// Route for sending OTP to the user's email
+router.post("/sendotp", sendotp)
+// Export the router for use in the main application
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken)
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
+
+
+//Route for post/like/comment
+router.post("/post", createPost);
+router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
+router.post("/comment",createComment);
+
+
+
+module.exports = router
